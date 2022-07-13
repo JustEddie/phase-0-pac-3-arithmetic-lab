d94d1d21ace1b55ce683c53e31c095051075d142
JustEddie <skdpwls830@gmail.com>
2022-07-13 14:26:21 +1000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+let result;
+
+function add(a,b) {
+    return result = a + b;
+}
+
+function subtract(a,b) {
+    return result = a - b;
+}
+
+function multiply(a,b) {
+    return result = a * b;
+}
+
+function divide(a,b) {
+    return result = a / b;
+}
+
+function increment(n) {
+    return result = n + 1;
+}
+
+function decrement(n) {
+    return result = n - 1;
+}
+
+function makeInt(n) {
+    return result = parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+    return result = parseFloat(n);
+}
